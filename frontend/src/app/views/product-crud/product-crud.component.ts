07d0e40c937ea676f1b4caacbd83c208c879fbe4
Igor Maciel <42256168+iigormr@users.noreply.github.com>
2021-12-31 16:47:36 -0300

    Criação de Interfaces e Bindings

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-product-crud',
-  templateUrl: './product-crud.component.html',
-  styleUrls: ['./product-crud.component.css']
+    selector: 'app-product-crud',
+    templateUrl: './product-crud.component.html',
+    styleUrls: ['./product-crud.component.css']
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    navigateToProductCreate(): void {
+        this.router.navigate(['/products/create']);
+    }
 }
